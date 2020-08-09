c92650a32a59e4b82fd9bd75d9eb91d98000fe64
Jesus Ramirez <jesusdevelp@gmail.com>
2020-08-09 11:21:01 -0500

    Routing e usuarios

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ console.log("Comenzando File Send");
 // Puerto de la app
 const port = process.env.PORT || 4000;
 
+// Rutas de la app
+app.use("/api/usuarios", require("./routes/usuarios"));
+
 // Arrancar la app
 app.listen(port, "0.0.0.0", () => {
   console.log(`El servidor esta funcionando en el puerto ${port}`);
