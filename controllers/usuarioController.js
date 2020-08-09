7fb007586056d6d82a627f2b3db3b1dc1f2d21f7
Jesus Ramirez <jesusdevelp@gmail.com>
2020-08-09 12:04:45 -0500

    Evitando registros duplicados

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -3,7 +3,16 @@ const Usuario = require("../models/Usuario");
 exports.nuevoUsuario = async (req, res) => {
   //   console.log(req.body);
 
-  const usuario = await new Usuario(req.body);
+  // Verificar si el usuario ya estuvo registrado
+  const { email } = req.body;
+
+  let usuario = await Usuario.findOne({ email });
+
+  if (usuario) {
+    return res.status(400).json({ msg: "EL usuario ya esta registrado" });
+  }
+
+  usuario = await new Usuario(req.body);
   usuario.save();
   res.json({ msg: "Usuario Creado Correctamente" });
 };
