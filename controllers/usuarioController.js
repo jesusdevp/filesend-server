99145e754317b5cfee5128e9321447565086dce3
Jesus Ramirez <jesusdevelp@gmail.com>
2020-08-09 12:23:39 -0500

    Hasheando Password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,10 +1,11 @@
 const Usuario = require("../models/Usuario");
+const bcrypt = require("bcrypt");
 
 exports.nuevoUsuario = async (req, res) => {
   //   console.log(req.body);
 
   // Verificar si el usuario ya estuvo registrado
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   let usuario = await Usuario.findOne({ email });
 
@@ -12,7 +13,17 @@ exports.nuevoUsuario = async (req, res) => {
     return res.status(400).json({ msg: "EL usuario ya esta registrado" });
   }
 
-  usuario = await new Usuario(req.body);
-  usuario.save();
-  res.json({ msg: "Usuario Creado Correctamente" });
+  // Crear un nuevo usuario
+  usuario = new Usuario(req.body);
+
+  // Hashear password
+  const salt = await bcrypt.genSalt(10);
+  usuario.password = await bcrypt.hash(password, salt);
+
+  try {
+    await usuario.save();
+    res.json({ msg: "Usuario Creado Correctamente" });
+  } catch (error) {
+    console.log(error);
+  }
 };
