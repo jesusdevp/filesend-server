275e1f3bb8baa55226caebbdfbc5d2583aa3a17d
Jesus Ramirez <jesusdevelp@gmail.com>
2020-09-30 16:55:33 -0500

    Creando Schema de enlaces y guardando en la BD

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,3 +1,21 @@
+const Enlaces = require("../models/Enlace");
+const shortid = require("shortid");
+
 exports.nuevoEnlace = async (req, res, next) => {
-  console.log("Desde nuevo enlace ");
+  // Crear un objeto de enlace
+  const { nombre_original, password } = req.body;
+  const enlace = new Enlaces();
+  enlace.url = shortid.generate();
+  enlace.nombre = shortid.generate();
+  enlace.nombre_original = nombre_original;
+  enlace.password = password;
+
+  // Almacena en BD
+  try {
+    await enlace.save();
+    res.json({ msg: `${enlace.url}` });
+    next();
+  } catch (error) {
+    console.log(error);
+  }
 };
