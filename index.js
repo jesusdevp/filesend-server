8eb2f431212076db17a93f7e861da44aa3764ef4
Jesus Ramirez <jesusdevelp@gmail.com>
2020-10-23 23:17:24 -0500

    Se agrego carpeta uploads

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const app = express();
 // COnectar a la base de datos
 conectarDB();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+
+  app.options("*", (req, res) => {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+    );
+    res.send();
+  });
+});
+
 // Habilitar Cors
 const opcionesCors = {
   origin: process.env.FRONTEND_URL,
